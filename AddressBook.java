69a8983906ad5a2793c66eccb7d0e73bfab0f871
Suraj <surajjadhav9741@gmail.com>
2020-10-20 01:23:10 +0530

    Add : Update and sort Function

--- AddressBook.java
+++ AddressBook.java
@@ -34,10 +34,12 @@ class AddressBook
   
     public void printValues() 
     { 
+		System.out.println("++++++++++++++++++++++++ Address Book Data ++++++++++++++++++++++++");
+		System.out.println("\tId\tname\tAddress\t City\tstate\tZip\tphone");
         for (int i = 0; i < list.size(); i++) 
         {
             Data data = list.get(i); 
-			System.out.println(data.Id+" "+data.name+" "+data.Address+" "+data.City+" "+data.state+" "+data.Zip+" "+data.phone); 
+			System.out.println("\t"+data.Id+"\t"+data.name+"\t"+data.Address+"\t"+data.City+"\t"+data.state+"\t"+data.Zip+"\t"+data.phone); 
         } 
     }
 
@@ -61,19 +63,131 @@ class AddressBook
 			int Zip = sc.nextInt();
 			System.out.println("Enter the Mobile Number :");
 			long phone = sc.nextLong();
+			addValues(Id, name, Address, City, state, Zip, phone);
+			System.out.println("Person Data Added successfully.\n");
 			System.out.println("Do you want to Add anather Person then enter (Y/N):");
 			check = sc.next();
-			n++;
-			addValues(Id, name, Address, City, state, Zip, phone); 
+			n++; 
 		}
 	}
 	public void deleteDate(int AcceptUserInt){
-		//this.AcceptUserInt=AcceptUserInt;
 		for (Data P : list){
 				if(P.Id==AcceptUserInt){
 					list.remove(P);
 				}
 			}
+	}
+	public void Update(){
+		int UserID = 0;
+		int Case = 0;
+		System.out.println("\n\n");
+		System.out.println("Enter id You want to update :");
+		UserID = sc.nextInt();
+		check="Y";
+		for (Data P : list){
+				if(P.Id==UserID){
+					while(check.equals("Y")||check.equals("y")){
+						System.out.println("\n\n");
+						System.out.println("What You Want To Update\n1.Name\n2.Address\n3.City\n4.State\n5.ZipCode\n6.Mobile Number");
+						Case = sc.nextInt();
+						switch (Case){
+							case 1:
+								System.out.println("Enter the Name :");
+								String UpdateName = sc.next();
+								P.name=UpdateName;
+								System.out.println("Name Updated successfully");
+							break;
+							case 2:
+								System.out.println("Enter the Address :");
+								String UpdateAddress = sc.next();
+								P.Address=UpdateAddress;
+								System.out.println("Address Updated successfully");
+							break;
+							case 3:
+								System.out.println("Enter the City :");
+								String UpdateCity = sc.next();
+								P.City=UpdateCity;
+								System.out.println("City Updated successfully");
+							break;
+							case 4:
+								System.out.println("Enter the State :");
+								String UpdateState = sc.next();
+								P.state=UpdateState;
+								System.out.println("State Updated successfully");
+							break;
+							case 5:
+								System.out.println("Enter the ZipCode :");
+								int UpdateZipCode = sc.nextInt();
+								P.Zip=UpdateZipCode;
+								System.out.println("Zip Code Updated successfully");
+							break;
+							case 6:
+								System.out.println("Enter the Mobile Number :");
+								long Updatephone = sc.nextLong();
+								P.phone=Updatephone;
+								System.out.println("Mobile Number Updated successfully");
+							break;
+						}
+						System.out.println("Do you want to Update anather value then enter (Y/N):");
+						check = sc.next();
+					}
+				}
+			}
+	}
+	
+	public void sortByName(){
+		ArrayList<String> SortName=new ArrayList<>();
+		for (Data P : list){
+				SortName.add(P.name);
+			}
+		Collections.sort(SortName);
+		System.out.println("++++++++++++++++++++++++ After Sorting by Name ++++++++++++++++++++++++");
+		System.out.println("\tId\tname\tAddress\t City\tstate\tZip\tphone");
+		for(String A : SortName){
+			for (Data P : list){
+				if(P.name.equals(A)){
+					System.out.println("\t"+P.Id+"\t"+P.name+"\t"+P.Address+"\t"+P.City+"\t"+P.state+"\t"+P.Zip+"\t"+P.phone);
+				}
+			}
+		}
+	}
+	
+	public void sortByZip(){
+		ArrayList<Integer> SortZip=new ArrayList<>();
+		for (Data P : list){
+				SortZip.add(P.Zip);
+			}
+		Collections.sort(SortZip);
+		System.out.println("++++++++++++++++++++++++ After Sorting by Zip Code ++++++++++++++++++++++++");
+		System.out.println("\tId\tname\tAddress\t City\tstate\tZip\tphone");
+			for(int A : SortZip){
+				for (Data P : list){
+					if(P.Zip==A){
+						System.out.println("\t"+P.Id+"\t"+P.name+"\t"+P.Address+"\t"+P.City+"\t"+P.state+"\t"+P.Zip+"\t"+P.phone);
+					}
+				}
+			}
+		
+	}
+	
+	public void sort(){
+		int Case=0;
+		System.out.println("\n\n");
+		System.out.println("1.Sort by Name\n2.Sort by Zip Code");
+		Case = sc.nextInt();
+		switch(Case){
+			case 1:
+				System.out.println("\n\n");
+				sortByName();
+				System.out.println("\n\n");
+			break;
+			case 2:
+				System.out.println("\n\n");
+				sortByZip();
+				System.out.println("\n\n");
+			break;
+		}
+		
 	}
 	
   
@@ -84,23 +198,31 @@ class AddressBook
 		int AcceptUserInt=0;
 		AddressBook custom = new AddressBook();
 		do{
-			System.out.println("1.Add Person \n2.Display Person\n3.Delete Person\n4.Exit");
+			System.out.println("\n\n");
+			System.out.println("1.Add Person\n2.Display Person\n3.Delete Person\n4.Update\n5.Sort Data\n6.Exit");
+			System.out.println("Enter Your Choice :");
 			cas = custom.sc.nextInt();
 				switch (cas){
 					case 1:
 						custom.NewPerson();
 					break;
 					case 2:
-						System.out.println("\n\n\n");
+						System.out.println("\n\n");
 						custom.printValues();
-						System.out.println("\n\n\n");
 					break;
 					case 3:
+						System.out.println("\n\n");
 						System.out.println("Enter the id for delete :");
 						AcceptUserInt = custom.sc.nextInt();
 						custom.deleteDate(AcceptUserInt);
 					break;
+					case 4:
+						custom.Update();
+					break;
+					case 5:
+						custom.sort();
+					break;
 				}
-		}while(cas!=4);
+		}while(cas!=6);
     } 
 } 
