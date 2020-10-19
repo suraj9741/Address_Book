d7a21dee6b64ec407da199dc5c354da8d41e7af9
Suraj <surajjadhav9741@gmail.com>
2020-10-19 22:13:35 +0530

    Add : search and delete Person data

--- AddressBook.java
+++ AddressBook.java
@@ -3,75 +3,104 @@ import java.util.*;
   
 class AddressBook 
 { 
-    //int n;  
+    //int n;
+	public String check="Y";
+	public Scanner sc = new Scanner(System.in);
     class Data 
     { 
-        int roll; 
+        int Id; 
         String name; 
-        int marks; 
-        long phone; 
-        Data(int roll, String name, int marks, long phone) 
+        int Zip; 
+        long phone;
+		String Address;
+		String City; 
+		String state;
+        Data(int Id, String name, String Address, String City, String state, int Zip, long phone) 
         { 
-            this.roll = roll; 
-            this.name = name; 
-            this.marks = marks; 
+            this.Id = Id; 
+            this.name = name;
+			this.Address = Address;
+			this.City = City;
+			this.state = state;
+            this.Zip = Zip; 
             this.phone = phone; 
         } 
-    } 
-	public ArrayList<Data> list=new ArrayList<>();
-	public void addValues(int roll, String name, int marks, long phone) 
-    {   
-        list.add(new Data(roll, name, marks, phone));
     }
-	public void print(int n){
-		printValues(list,n);
-	}
+	public ArrayList<Data> list=new ArrayList<>();
+	public void addValues(int Id, String name, String Address, String City, String state, int Zip, long phone) 
+    {   
+        list.add(new Data(Id, name, Address, City, state, Zip, phone));
+    }
   
-    public void printValues(ArrayList<Data> list , int n) 
+    public void printValues() 
     { 
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < list.size(); i++) 
         {
             Data data = list.get(i); 
-			System.out.println(data.roll+" "+data.name+" "+data.marks+" "+data.phone); 
+			System.out.println(data.Id+" "+data.name+" "+data.Address+" "+data.City+" "+data.state+" "+data.Zip+" "+data.phone); 
         } 
-    } 
+    }
+
+	public void NewPerson(){
+		int n=0;
+		check="Y";
+		while(check.equals("Y")||check.equals("y")){
+			System.out.println("\n\n");
+			System.out.println("Enter the person ID number :");
+			int Id = sc.nextInt();
+					sc.nextLine();
+			System.out.println("Enter the Name :");
+			String name = sc.nextLine();
+			System.out.println("Enter the address:");
+			String Address = sc.next();
+			System.out.println("Enter the city:");
+			String City = sc.next();
+			System.out.println("Enter the State:");
+			String state = sc.next();
+			System.out.println("Enter the ZipCode :");
+			int Zip = sc.nextInt();
+			System.out.println("Enter the Mobile Number :");
+			long phone = sc.nextLong();
+			System.out.println("Do you want to Add anather Person then enter (Y/N):");
+			check = sc.next();
+			n++;
+			addValues(Id, name, Address, City, state, Zip, phone); 
+		}
+	}
+	public void deleteDate(int AcceptUserInt){
+		//this.AcceptUserInt=AcceptUserInt;
+		for (Data P : list){
+				if(P.Id==AcceptUserInt){
+					list.remove(P);
+				}
+			}
+	}
+	
   
     public static void main(String args[]) 
     { 
-		int check=1;
-		int n=0;
-		int cas=0;
-		Scanner sc = new Scanner(System.in);
+		int cas = 0;
+		String AcceptUser = " ";
+		int AcceptUserInt=0;
 		AddressBook custom = new AddressBook();
 		do{
-			System.out.println("1.Add Person \n2.Display Person\n3.Exit");
-			cas = sc.nextInt();
+			System.out.println("1.Add Person \n2.Display Person\n3.Delete Person\n4.Exit");
+			cas = custom.sc.nextInt();
 				switch (cas){
 					case 1:
-						check=1;
-						while(check==1){
-							System.out.println("\n\n");
-							System.out.println("Enter the roll number :");
-							int roll = sc.nextInt();
-									sc.nextLine();
-							System.out.println("Enter the Name :");
-							String name = sc.nextLine();
-							System.out.println("Enter the Marks :");
-							int marks = sc.nextInt();
-							System.out.println("Enter the Mobile Number :");
-							long phone = sc.nextLong();
-							System.out.println("Do you want to Add anather Person then enter (1):");
-							check = sc.nextInt();
-							n++;
-							custom.addValues(roll, name, marks, phone); 
-						}
+						custom.NewPerson();
 					break;
 					case 2:
 						System.out.println("\n\n\n");
-						custom.print(n);
+						custom.printValues();
 						System.out.println("\n\n\n");
 					break;
+					case 3:
+						System.out.println("Enter the id for delete :");
+						AcceptUserInt = custom.sc.nextInt();
+						custom.deleteDate(AcceptUserInt);
+					break;
 				}
-		}while(cas!=3);
+		}while(cas!=4);
     } 
 } 
